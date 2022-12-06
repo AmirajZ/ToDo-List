0746d9785ff84d28237afe8d6364969220ce415f
Amirajsinh Zala <108674145+AmirajZ@users.noreply.github.com>
2022-12-06 19:07:01 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Navbar from './Components/Navbar';
+import TextArea from './Components/TextArea';
+
+
+
+function App(){ 
+  return(
+    <>
+      <Navbar title="To--DO"></Navbar>
+      <TextArea title="Lets Make This Day Productive"></TextArea>
+    </>
+  )
+}
+export default App;
